24a4d7c2ac3ff45b36f0d1d9a675393df52a24b6
Rosano Coutinho <pub@xeari.com>
2021-07-05 12:32:08 -0300

    Require _OLSKModulesDirPrefix

--- controller.js
+++ controller.js
@@ -8,6 +8,7 @@ const mod = {
 			OLSKRouteFunction (req, res, next) {
 				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'main'), Object.assign({
 					ROCOBulletinProject: Math.random().toString(),
+					_OLSKModulesDirPrefix: '',
 				}, Object.fromEntries(Array.from((new URLSearchParams(req.query)).entries()).filter(function ([key, value]) {
 					return value !== 'undefined';
 				}))));
